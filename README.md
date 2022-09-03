214bf40d5a873e8ea87788ae92c32e139d17cdd7
Michael Nash <michaelnash.work@gmail.com>
2022-09-03 12:28:19 +0100

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DokkanAPI
+
+node server.ts
---
+++ server.ts
@@ -0,0 +1,26 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    character: number
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(8080);
+console.log('Running a GraphQL API server at http://localhost:8080/graphql');
