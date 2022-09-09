e358347145b91af87aead3ce1f185478c3c69332
Michael Nash <michaelnash.work@gmail.com>
2022-09-09 21:13:49 +0100

    added all character fields

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ let express = require('express');
 let { graphqlHTTP } = require('express-graphql');
 let { buildSchema } = require('graphql');
 var graphql = require('graphql');
-import { GraphQLList } from "graphql";
+import { GraphQLList, GraphQLString } from "graphql";
 import { Character, Rarities, Classes, Types, Transformation } from "./character";
 
 const characterData: any[] = require("../data/DokkancharacterData.json")
@@ -16,6 +16,34 @@ var characterType = new graphql.GraphQLObjectType({
     },
     name: { type: graphql.GraphQLString },
     title: { type: graphql.GraphQLString },
+    maxLevel: { type: graphql.GraphQLInt },
+    maxSALevel: { type: graphql.GraphQLInt },
+    rarity: { type: graphql.GraphQLString },
+    class: { type: graphql.GraphQLString },
+    type: { type: graphql.GraphQLString },
+    cost: { type: graphql.GraphQLInt },
+    imageURL: { type: graphql.GraphQLString },
+    leaderSkill: { type: graphql.GraphQLString },
+    superAttack: { type: graphql.GraphQLString },
+    ultraSuperAttack: { type: graphql.GraphQLString },
+    passive: { type: graphql.GraphQLString },
+    links: { type: graphql.GraphQLList(GraphQLString) },
+    categories: { type: graphql.GraphQLList(GraphQLString) },
+    kiMeter: { type: graphql.GraphQLList(GraphQLString) },
+    baseHP: { type: graphql.GraphQLInt },
+    maxLevelHP: { type: graphql.GraphQLInt },
+    freeDupeHP: { type: graphql.GraphQLInt },
+    rainbowHP: { type: graphql.GraphQLInt },
+    baseAttack: { type: graphql.GraphQLInt },
+    maxLevelAttack: { type: graphql.GraphQLInt },
+    freeDupeAttack: { type: graphql.GraphQLInt },
+    rainbowAttack: { type: graphql.GraphQLInt },
+    baseDefence: { type: graphql.GraphQLInt },
+    maxDefence: { type: graphql.GraphQLInt },
+    freeDupeDefence: { type: graphql.GraphQLInt },
+    rainbowDefence: { type: graphql.GraphQLInt },
+    kiMultiplier: { type: graphql.GraphQLString },
+    transformations: { type: graphql.GraphQLList(GraphQLString) }
   }
 });
 
